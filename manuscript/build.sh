664b92efc34f89e166fbd9207606910a778d62f5
Barney Potter <barney.potter.24@gmail.com>
2019-01-30 11:57:00 -0800

    Add manuscript directory with final figures.

---
+++ manuscript/build.sh
@@ -0,0 +1,15 @@
+rm *.log
+rm *.aux
+rm *.out
+rm *.bbl
+rm *.bcf
+rm *.blg
+rm .DS_Store
+rm h3n2_reassortment.pdf
+
+pdflatex h3n2_reassortment.tex
+bibtex h3n2_reassortment
+pdflatex h3n2_reassortment.tex
+pdflatex h3n2_reassortment.tex
+pdflatex h3n2_reassortment.tex
+open h3n2_reassortment.pdf
